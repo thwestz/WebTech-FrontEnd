595254b9b7f4f626b98c968b2abf6e9b7dbdcc25
THWest <thwest001@hotmail.com>
2017-12-15 14:01:30 +0700

    update

--- src/app/services/event.service.ts
+++ src/app/services/event.service.ts
@@ -1,32 +1,50 @@
 import { Injectable } from "@angular/core";
 
-import { User } from "../Models/user.model";
+import { Event } from "../Models/event/event.model";
 import { Observable } from "rxjs/Observable";
 import { HttpClient } from "@angular/common/http";
-import { HttpHeaders } from "@angular/common/http";
-import { LocalStorageService } from "ngx-webstorage";
-import { Event } from "../Models/Event/event.model";
 import { Session } from "../Models/session.model";
+import { LocalStorageService } from "ngx-webstorage";
 
 
 
 @Injectable()
 export class EventService {
-
+    private session: Session;
     private basePath: string = `http://localhost:5000/event`;
-    private session : Session
-    constructor(
-        private http: HttpClient
-        , private local: LocalStorageService) { }
 
+    constructor(
+        private http: HttpClient, private locals: LocalStorageService) { }
+
+
+    getEvent(): Observable<Event[]> {
+        return this.http.get<Event[]>(`${this.basePath}`);
+    }
+    //
+    getEventByStatus(status: number): Observable<Event[]> {
+        return this.http.get<Event[]>(`${this.basePath}/status/${status}`);
+    }
+    getEventByID(id:string): Observable<Event> {
+        return this.http.get<Event>(`${this.basePath}/${id}`);
+    }
+
+    getEventByUserID() : Observable<Event[]>{
+        this.getSession();
+        return this.http.get<Event[]>(`${this.basePath}/owner/${this.session.uid}`);
+    }
+
+    update(event: Event): Observable<Event> {
+
+        return this.http.put<Event>(`${this.basePath}/update`, (event));
+    }
 
     create(event: Event): Observable<Event> {
         this.getSession();
         event.userID = this.session.uid;
-        return this.http.post<Event>(`${this.basePath}/create`, (event))
+        return this.http.post<Event>(`${this.basePath}/create`, (event));
     }
 
     getSession() {
-        this.session = this.local.retrieve('token');
+        this.session = this.locals.retrieve('token');
     }
 }
