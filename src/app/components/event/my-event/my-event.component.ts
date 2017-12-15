c0d99b7c3ef92e2d787b29fbf78909b41ac4ac4f
THWest <thwest001@hotmail.com>
2017-12-15 17:01:44 +0700

    update summary

--- src/app/components/event/my-event/my-event.component.ts
+++ src/app/components/event/my-event/my-event.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { EventService } from '../../../services/event.service';
 import { Event } from '../../../Models/event/event.model';
+import { Session } from '../../../Models/session.model';
+import { LocalStorageService } from 'ngx-webstorage';
 
 @Component({
   selector: 'app-my-event',
@@ -12,11 +14,15 @@ export class MyEventComponent implements OnInit {
   eventList: Event[];
   eventSearch: Event[];
   loading: boolean;
-  eventDetail : Event;
-  constructor(private route: ActivatedRoute, private eventService: EventService) { }
+  eventDetail: Event;
+  session: Session;
+  barcode: string;
+  constructor(private route: ActivatedRoute, private eventService: EventService,
+    private locals: LocalStorageService) { }
 
   ngOnInit() {
     this.loading = true;
+    this.session = this.locals.retrieve('token');
     this.route.params.subscribe(id => {
       this.eventService.getEventByUserIDRegisted().subscribe(res => {
         this.eventList = res;
@@ -24,6 +30,12 @@ export class MyEventComponent implements OnInit {
         this.loading = false;
       })
     });
+  }
+
+  isEvent(event ) {
+
+    this.eventDetail = event
+    this.barcode = `${this.session.uid}?${this.eventDetail._id}`
   }
 
 
